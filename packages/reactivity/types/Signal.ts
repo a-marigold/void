95eb52503afacb3a043b7b029b3d44061f9985ed
NextKolya <phelixgluy@gmail.com>
2025-12-08 20:45:35 +0600

    Add types for Signal

---
+++ packages/reactivity/types/Signal.ts
@@ -0,0 +1,8 @@
+import type { Computation } from './Computation';
+
+export type SignalGetter<T> = () => T;
+export type SignalSetter<T> = (newValue: T) => T;
+
+export type Subscribers = Set<Computation>;
+
+export type Signal<T> = { get: SignalGetter<T>; set: SignalSetter<T> };
