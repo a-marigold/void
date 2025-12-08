d52049e008e9e4f7ae358fc7a3c668e51fbb99d8
NextKolya <phelixgluy@gmail.com>
2025-12-08 20:05:55 +0600

    Add rollup and rollup.config

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'rollup';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+import npmPackage from './package.json' with {type: 'json'};
+
+export default defineConfig([
+    {
+        input: 'packages/index.ts',
+
+        output: [{ file: npmPackage.module, format: 'esm' }],
+
+        plugins: [
+            resolve(),
+            typescript({ tsconfig: './tsconfig.json', declaration: false }),
+        ],
+    },
+    {
+        input: 'packages/index.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+
+        plugins: [dts()],
+    },
+]);
