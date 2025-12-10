8e837f41a8ee8c84ca8d45c5b2a5784b8702e032
NextKolya <phelixgluy@gmail.com>
2025-12-10 19:24:05 +0600

    Add rollup terser plugin for minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'rollup';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser'
 import dts from 'rollup-plugin-dts';
 
 import npmPackage from './package.json' with {type: 'json'};
@@ -14,6 +15,7 @@ export default defineConfig([
         plugins: [
             resolve(),
             typescript({ tsconfig: './tsconfig.json', declaration: false }),
+            terser()
         ],
     },
     {
