88d90c84114a7b9972dad553891dc66de00373f9
NextKolya <phelixgluy@gmail.com>
2026-02-12 21:40:48 +0600

    add resetContext testing util

---
+++ packages/void/src/reactivity/__tests__/testingUtils.ts
@@ -0,0 +1,17 @@
+import { context } from '../context';
+
+/**
+ *
+ *
+ * Resets all the {@link context} properties to their initial values
+ *
+ */
+export const resetContext = (): void => {
+    context.currentSubscriber = null;
+
+    context.isScheduled = false;
+
+    context.scheduledSubscribers.clear();
+
+    context.scheduledSignals.clear();
+};
