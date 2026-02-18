3145bf44e2fe30cc2d886db7edbd68c945968249
NextKolya <phelixgluy@gmail.com>
2026-02-18 20:36:42 +0600

    add PUNCTUATORS in constants

--- packages/void/src/compiler/preprocessor/constants.ts
+++ packages/void/src/compiler/preprocessor/constants.ts
@@ -15,7 +15,7 @@ import type { VoidKeyword } from './types';
  *
  */
 
-export const IDENTIFIER_START_REGEXP = /^[\p{ID_Start}_$]$/u;
+export const IDENTIFIER_START_REGEXP = /^[\p{ID_Start}_$]+$/u;
 
 /**
  * RegExp that allows one ecmascript identifer character.
@@ -23,20 +23,66 @@ export const IDENTIFIER_START_REGEXP = /^[\p{ID_Start}_$]$/u;
  * @example
  *
  * ```typescript
- * IDENTIFIER_START_REGEXP.test('a'); // true
- * IDENTIFIER_START_REGEXP.test('_'); // true
- * IDENTIFIER_START_REGEXP.test('$'); // true
- * IDENTIFIER_START_REGEXP.test('1'); // true
+ * IDENTIFIER_PART_REGEXP.test('a'); // true
+ * IDENTIFIER_PART_REGEXP.test('_'); // true
+ * IDENTIFIER_PART_REGEXP.test('$'); // true
+ * IDENTIFIER_PART_REGEXP.test('1'); // true
  * ```
  *
  */
 
-export const IDENTIFIER_REGEXP = /^[\p{ID_Continue}$]$/u;
+export const IDENTIFIER_PART_REGEXP = /^[\p{ID_Continue}$]+$/u;
+
+/**
+ * `Set` with symbols that can interrupt an identifier.
+ *
+ * @example
+ * ```typescript
+ * 'identif!ier'
+ *         ^
+ *         | --- Interruption
+ * ```
+ */
+export const PUNCTUATORS = new Set([
+    '{',
+    '}',
+    '(',
+    ')',
+    '[',
+    ']',
+
+    '.',
+    ',',
+
+    ':',
+
+    '=',
+
+    '<',
+    '>',
+    ';',
+    '!',
+    '?',
+    '|',
+    '~',
+    '&',
+    '+',
+
+    '-',
+    '*',
+    '/',
+    '*',
+    '^',
+    "'",
+    '"',
+    '`',
+]);
 
 export const EFFECT_PREFIX = '_$effect';
 
 export const VOID_KEYWORDS: { [K in VoidKeyword]: VoidKeyword } = {
     signal: 'signal',
+
     effect: 'effect',
     computation: 'computation',
 };
