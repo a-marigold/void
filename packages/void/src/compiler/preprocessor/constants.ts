e1428546aca9f021e6a9d95b5a35f7ff1649ff54
NextKolya <phelixgluy@gmail.com>
2026-02-19 16:56:45 +0600

    add keyword labels in preprocessor constants

--- packages/void/src/compiler/preprocessor/constants.ts
+++ packages/void/src/compiler/preprocessor/constants.ts
@@ -64,7 +64,77 @@ export const PUNCTUATORS = new Set([
     '#',
 ]);
 
-export const EFFECT_PREFIX = '_$effect';
+/**
+ *
+ * Used to identify does the next line contain `signal` declaration.
+ *
+ *
+ * @example
+ *
+ * `void-js`
+ * ```typescript
+ * signal count = 0;
+ * ```
+ *
+ * Output:
+ *
+ * ```typescript
+ * const _$signal = 1; // preprocessor added this line
+ *
+ * _$signal;
+ * let count = 0; // this was a `signal` in `void-js` source file
+ * ```
+ *
+ *
+ */
+
+export const SIGNAL_LABEL = '_$signal';
+
+/**
+ *
+ * Used to identify does the next line contain `effect`.
+ *
+ * @example
+ *
+ * `void-js`:
+ * ```typescript
+ * effect () => {
+ *   console.log('hello');
+ * };
+ * ```
+ * Output:
+ * ```typescript
+ * const _$effect = 1; // preprocessor added this line
+ *
+ * _$effect;
+ * () => { console.log('Hello'); }; // this was an `effect` in `void-js` source file
+ * ```
+ */
+
+export const EFFECT_LABEL = '_$effect';
+
+/**
+ *
+ * Used to identify does the next line contain `computation`.
+ *
+ * @example
+ *
+ *
+ * `void-js`:
+ *
+ * ```typescript
+ * computation doubled = () => 2;
+ * ```
+ * Output:
+ * ```typescript
+ * const _$computation = 1; // preprocessor added this line
+ *
+ * _$computation;
+ * const dobuled = () => 2; // this was a `computation in `void-js` source file
+ * ```
+ */
+
+export const COMPUTATION_LABEL = '_$computation';
 
 export const VOID_KEYWORDS = new Set<VoidKeyword>([
     'signal',
