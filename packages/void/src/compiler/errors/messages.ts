0af15cb1d9efedab8bce35bcb10255099058570c
NextKolya <phelixgluy@gmail.com>
2026-02-20 20:10:32 +0600

    update compile error messages to functions

--- packages/void/src/compiler/errors/messages.ts
+++ packages/void/src/compiler/errors/messages.ts
@@ -1,10 +1,19 @@
 /**
  *
  * Object with messages of errors that appear while `void-js` file is compiling.
+ *
+ *
+ *
+ *
+ *
  */
 
 export const compileErrors = {
-    SIGNAL_WITHOUT_IDENTIFIER: 'Identifier of signal expected',
+    SIGNAL_WITHOUT_IDENTIFIER: () => 'Identifier of signal expected',
 
-    COMPUTATION_WITHOUT_IDENTIFIER: 'Identifier of computation expected',
+    COMPUTATION_WITHOUT_IDENTIFIER: () => 'Identifier of computation expected',
+
+    VOID_KEYWORD_AS_VARIABLE_NAME: (keyword: string) =>
+        keyword +
+        "is a 'void-js' keyword and is not allowed as variable declaration name.",
 } as const;
