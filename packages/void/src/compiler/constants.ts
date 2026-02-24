9b1f279f3377625e8e3a57e692800293807c8b1c
NextKolya <phelixgluy@gmail.com>
2026-02-24 22:03:37 +0600

    add reactivity api names constant in compiler

---
+++ packages/void/src/compiler/constants.ts
@@ -0,0 +1,38 @@
+import type { ReactivityApiName } from './types';
+
+/**
+ *
+ * Object with names of `void-js` reactivity API.
+ */
+export const REACITVITY_API_NAMES: {
+    [K in ReactivityApiName]: ReactivityApiName;
+} = {
+    /**
+     * Type of identifier that defined via `signal` keyword.
+     */
+    Signal: 'Signal',
+
+    /**
+     * `Signal.value` getter.
+     */
+    getValue: 'getValue',
+
+    /**
+     * `Signal.value` setter.
+     */
+    setValue: 'setValue',
+
+    /**
+     * `Signal.value` setter that returns previous value of a `Signal`.
+     */
+    postSetValue: 'postSetValue',
+
+    createEffect: 'createEffect',
+
+    createComputation: 'createComputation',
+
+    /**
+     * Function that computes a `Computation`.
+     */
+    compute: 'compute',
+};
