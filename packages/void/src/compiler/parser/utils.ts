5c25744d59f306451b51c5876599eb50c6850c8f
NextKolya <phelixgluy@gmail.com>
2026-02-26 23:11:58 +0600

    add createComputationDeclarator in parse utils

--- packages/void/src/compiler/parser/utils.ts
+++ packages/void/src/compiler/parser/utils.ts
@@ -1,21 +1,21 @@
 import * as types from '@babel/types';
-
 import type { VariableDeclarator, TSTypeAnnotation } from '@babel/types';
+
+import type { PreprocessResult } from '../preprocessor';
 
 import { CompileError, compileErrors } from '../errors';
 
 /**
  *
  *
+ *
  * #### Creates variable declarator for `signal` identifier from original identifier and original initial value.
  *
  *
  *
- *
- *
  * @param originalIdentifier Identifier (left hand side in variable declaration) from `void-js` source file.
- * @param initialValue Initial value of `signal` identifier from `void-js` source file.
- * @param signalTypeName Name of imported `Signal` type from runtime `void-js` API. This parameter is needed because of unique import names after preprocessing.
+ * @param initialValue Initial value of `signal` identifier.
+ * @param runtimeApiNames {@link PreprocessResult.runtimeApiNames}
  *
  * @returns `VariableDeclarator` for `babel` AST.
  *
@@ -23,11 +23,11 @@ import { CompileError, compileErrors } from '../errors';
 export const createSignalDeclarator = (
     originalIdentifier: types.VariableDeclarator['id'],
     initialValue: types.VariableDeclarator['init'],
-    signalTypeName: string,
+    runtimeApiNames: string,
 ): VariableDeclarator => {
     if (!initialValue) {
         throw new CompileError(
-            compileErrors.SIGNAL_WITHOUT_INITIAL_VALUE(),
+            compileErrors.KEYWORD_WITHOUT_INITIAL_VALUE('signal'),
 
             0,
 
@@ -36,7 +36,11 @@ export const createSignalDeclarator = (
     }
 
     if (originalIdentifier.type !== 'Identifier') {
-        throw new CompileError(compileErrors.SIGNAL_DESTRUCTURING(), 0, 0);
+        throw new CompileError(
+            compileErrors.KEYWORD_DESTRUCTURING('signal'),
+            0,
+            0,
+        );
     }
 
     const identifier = types.identifier(originalIdentifier.name);
@@ -47,7 +51,7 @@ export const createSignalDeclarator = (
 
     identifier.typeAnnotation = types.tsTypeAnnotation(
         types.tsTypeReference(
-            types.identifier(signalTypeName),
+            types.identifier(runtimeApiNames),
 
             originalTSType &&
                 types.tsTypeParameterInstantiation([originalTSType]),
@@ -70,5 +74,55 @@ export const createSignalDeclarator = (
                 types.cloneNode(initialValue),
             ),
         ]),
+    );
+};
+
+/**
+ *
+ * #### Creates `VariableDeclarator` for `computation` from original identifier and initial value (that is a function for `computation`).
+ *
+ * @param originalIdentifier Identifier of `computation`.
+ * @param initialValue Initial value of `computation` (usually that is a function).
+ * @param runtimeApiNames {@link PreprocessResult.runtimeApiNames} from preprocessor.
+ *
+ * @returns `VariableDeclaration` for `babel` AST.
+ */
+export const createComputationDeclarator = (
+    originalIdentifier: VariableDeclarator['id'],
+    initialValue: VariableDeclarator['init'],
+
+    runtimeApiNames: PreprocessResult['runtimeApiNames'],
+): VariableDeclarator => {
+    if (!initialValue) {
+        throw new CompileError(
+            compileErrors.KEYWORD_WITHOUT_INITIAL_VALUE('computation'),
+            0,
+            0,
+        );
+    }
+
+    if (originalIdentifier.type !== 'Identifier') {
+        throw new CompileError(
+            compileErrors.KEYWORD_DESTRUCTURING('computation'),
+            0,
+            0,
+        );
+    }
+
+    const originalTsType = (
+        originalIdentifier.typeAnnotation as TSTypeAnnotation | undefined
+    )?.typeAnnotation;
+
+    const createComputationCall = types.callExpression(
+        types.identifier(runtimeApiNames.get('createComputation') as string),
+        [types.cloneNode(initialValue)],
+    );
+    createComputationCall.typeParameters =
+        originalTsType &&
+        types.tsTypeParameterInstantiation([types.cloneNode(originalTsType)]);
+
+    return types.variableDeclarator(
+        types.identifier(originalIdentifier.name),
+        createComputationCall,
     );
 };
