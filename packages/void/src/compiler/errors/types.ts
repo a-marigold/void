319782620be9afd2285a3530e69efa878493a53e
NextKolya <phelixgluy@gmail.com>
2026-03-06 20:50:36 +0600

    add getIndexLocation util in errros

---
+++ packages/void/src/compiler/errors/types.ts
@@ -0,0 +1,19 @@
+export type NewLineIndexes = number[];
+
+/**
+ *
+ *
+ * Object with `line` (starts from 1) and `column` that is located in `line` (starts from 0)
+ */
+export type Location = {
+    /**
+     *
+     * Starts from 1.
+     */
+    line: number;
+
+    /**
+     * Start from 0.
+     */
+    column: number;
+};
--- packages/void/src/compiler/errors/utils.ts
+++ packages/void/src/compiler/errors/utils.ts
@@ -1,19 +1,23 @@
+import type { NewLineIndexes, Location } from './types';
+
 /**
  *
  * #### Returns an array with positions of `\n` characters.
- *
  * @param source String to be explored on new line positions.
+ *
+ *
+ *
  * @returns Array with positions of `\n` characters.
+ *
  *
  * @example
  *
  * ```typescript
- * getNewLinePositions('abc\ndef\njkl') // Output: `[3, 7]`
+ * getNewLineIndexes('abc\ndef\njkl') // Output: `[3, 7]`
  * ````
  */
-
-const getNewLinePositions = (source: string): number[] => {
-    const positions: number[] = [];
+export const getNewLineIndexes = (source: string): NewLineIndexes => {
+    const positions: NewLineIndexes = [];
 
     const sourceLength = source.length;
 
@@ -28,4 +32,36 @@ const getNewLinePositions = (source: string): number[] => {
     }
 
     return positions;
+};
+
+/**
+ *
+ * #### Returns object with `line` of `index` and `column` of `index` in the line, uses provided `newLineIndexes` of string.
+ *
+ * @param newLineIndexes Array with indexes derived via {@link getNewLineIndexes}
+ *
+ * @param index Index, location of which to be found.
+ *
+ *
+ */
+
+export const getIndexLocation = (
+    newLineIndexes: NewLineIndexes,
+
+    index: number,
+): Location => {
+    let lowBound = 0;
+
+    let highBound = newLineIndexes.length;
+
+    while (lowBound < highBound) {
+        const middleIndex = (lowBound + highBound) >> 1;
+        if (newLineIndexes[middleIndex] < index) {
+            lowBound = middleIndex + 1;
+        } else {
+            highBound = middleIndex;
+        }
+    }
+
+    return { line: lowBound + 1, column: index - lowBound - 1 };
 };
