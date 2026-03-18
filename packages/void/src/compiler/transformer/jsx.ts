ca53f2ffa6084522e6758e510a0dfac6da163c02
NextKolya <phelixgluy@gmail.com>
2026-03-18 20:30:21 +0600

    add generateChildPath function in transformer jsx logic

--- packages/void/src/compiler/transformer/jsx.ts
+++ packages/void/src/compiler/transformer/jsx.ts
@@ -1,4 +1,15 @@
-import type { JSXElement } from '@babel/types';
+import * as types from '@babel/types';
+import type {
+    Identifier,
+    MemberExpression,
+    JSXElement,
+    VariableDeclaration,
+    VariableDeclarator,
+} from '@babel/types';
+
+import type { PreprocessResult } from '../preprocessor';
+
+import { generateUniqueIdentifier } from '../preprocessor/utils';
 
 /**
  *
@@ -9,27 +20,54 @@ const NO_CHILDREN: readonly [] = [];
 
 /**
  *
- * #### Starting from children of a root element, walks all the JSX tree and transforms JSX to dom API calls.
+ * #### Generates DOM path to child from root in babel AST nodes.
  *
  *
  *
- * @param rootChildren {@link JSXElement.children} of root element.
+ * @param parentName Identifier name of parent element.
  *
+ * @param elementIndex Index of place of child in parent children.
+ *
+ * @returns {Identifier | MemberExpression} {@link Identifier} with `parentName` if `elementIndex` is `0`. Otherwise returns `MemberExpression` with path from parent to child.
+ *
+ * @example
+ *
+ * ```tsx
+ * <div>
+ *   <h1> H </h1>
+ *   <p> P </p>
+ * </div>
+ *
+ *
+ * generateChildPath('div', 2);
+ *
+ *
+ *  // Output (if generated via babel gen)
+ *
+ * `div.firstChild.nextSibling`; // `<p> </p>`
+ * ```
  */
-export const walkJSXPaths = (
-    rootChildren: JSXElement['children'] | typeof NO_CHILDREN,
-    path: number[] = [], // TODO: here
-): void => {
-    const rootChildrenLength = rootChildren.length;
+export const generateChildPath = (
+    parentName: string,
+    elementIndex: number,
+): Identifier | MemberExpression => {
+    let elementPath: Identifier | MemberExpression = types.memberExpression(
+        types.identifier(parentName),
 
-    let childIndex = 0;
+        types.identifier('firstChild'),
+    );
+    let pathIndex = 0;
+    while (pathIndex < elementIndex) {
+        elementPath = types.memberExpression(
+            elementPath,
+            types.identifier('nextSibling'),
+        );
 
-    while (childIndex < rootChildrenLength) {
-        const child = rootChildren[childIndex];
+        pathIndex++;
+    }
 
-        path[path.length] = childIndex;
-
-        walkJSXPaths('children' in child ? child.children : NO_CHILDREN, path);
+    return elementPath;
+};
 
         path.pop();
 
