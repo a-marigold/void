438b2e5046f8d8960aaeeb81610556afe7ac0de8
NextKolya <phelixgluy@gmail.com>
2026-03-21 21:36:15 +0600

    remove isUncapitalized with isLowerCase util

---
+++ packages/void/src/compiler/__tests__/utils/isLowerCase.test.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'bun:test';
+
+import { isLowerCase } from '../../utils/isLowerCase';
+
+describe('isLowerCase', () => {
+    it('should return `true` if the `string` is in lowercase and `false` if is in not', () => {
+        expect(isLowerCase('abc')).toBe(true);
+        expect(isLowerCase('aBc')).toBe(true);
+    });
+});
---
+++ packages/void/src/compiler/utils/isLowerCase.ts
@@ -0,0 +1,10 @@
+/**
+ *
+ * #### Checks is the string fully in lower case.
+ *
+ * @param string String to be checked
+ * @returns Boolean indicating is the `string` in lower case or is not.
+ */
+
+export const isLowerCase = (string: string): boolean =>
+    string === string.toLowerCase();
