515e444c1cb41f17e93613bb5c7b47d5b1ad1111
NextKolya <phelixgluy@gmail.com>
2026-03-21 20:42:57 +0600

    complete analyzeJsx template tests

--- packages/void/src/compiler/__tests__/transformer/jsx/analyzeJsx.test.ts
+++ packages/void/src/compiler/__tests__/transformer/jsx/analyzeJsx.test.ts
@@ -39,45 +39,100 @@ describe('analyzeJsx', () => {
               "
             `);
         });
-    });
 
-    it('should skip nested fragments and have errors with them', () => {
-        const errors: CompileError[] = [];
+        it('should skip nested fragments and have errors with them', () => {
+            const errors: CompileError[] = [];
 
-        expect(
-            analyzeJsx(
-                parseExpression(
-                    '<div> <> 1 </>  <> <> 2 </> </> <span> <> 3 </> </span> </div>',
+            expect(
+                analyzeJsx(
+                    parseExpression(
+                        '<div> <> 1 </>  <> <> 2 </> </> <span> <> 3 </> </span> </div>',
 
-                    { plugins: ['jsx'] },
-                ) as JSXElement,
+                        { plugins: ['jsx'] },
+                    ) as JSXElement,
 
-                __emptyTraceMap__,
+                    __emptyTraceMap__,
 
-                errors,
-            ).templateString,
-        ).toMatchInlineSnapshot(`"<div>    <span>  </span> </div>"`);
+                    errors,
+                ).templateString,
+            ).toMatchInlineSnapshot(`"<div>    <span>  </span> </div>"`);
 
-        expect(errors.map((error) => error.message)).toMatchInlineSnapshot(`
+            expect(errors.map((error) => error.message)).toMatchInlineSnapshot(`
           [
             "JSX fragment should not appear here.",
             "JSX fragment should not appear here.",
             "JSX fragment should not appear here.",
           ]
         `);
-    });
+        });
 
-    it('should generate template with inclusion the root if it is not a fragment', () => {
-        expect(
-            analyzeJsx(
-                parseExpression(`<div> <span> </span> <div> </div> </div>`, {
-                    plugins: ['jsx'],
-                }) as JSXElement,
+        it('should generate template with inclusion of the root if it is not a fragment', () => {
+            expect(
+                analyzeJsx(
+                    parseExpression(
+                        `<div> <span> </span> <div> </div> </div>`,
+                        {
+                            plugins: ['jsx'],
+                        },
+                    ) as JSXElement,
 
-                __emptyTraceMap__,
+                    __emptyTraceMap__,
 
-                [],
-            ).templateString,
-        ).toMatchInlineSnapshot(`"<div> <span> </span> <div> </div> </div>"`);
+                    [],
+                ).templateString,
+            ).toMatchInlineSnapshot(
+                `"<div> <span> </span> <div> </div> </div>"`,
+            );
+        });
+
+        it('should generate HTML comments for JSX expressions', () => {
+            expect(
+                analyzeJsx(
+                    parseExpression(
+                        `<div>
+  {'abc'}
+  {1231616}
+  <span> {'count'} </span>
+  <p> {<div> </div>} </p>
+</div>`,
+                        { plugins: ['jsx'] },
+                    ) as JSXElement,
+                    __emptyTraceMap__,
+
+                    [],
+                ).templateString,
+            ).toMatchInlineSnapshot(`
+              "<div>
+                <!---->
+                <!---->
+                <span> <!----> </span>
+                <p> <!----> </p>
+              </div>"
+            `);
+
+            expect(
+                analyzeJsx(
+                    parseExpression(
+                        `<>
+  {'abc'}
+  {1231616}
+  <span> {'count'} </span>
+  <p> {<div> </div>} </p>
+</>`,
+                        { plugins: ['jsx'] },
+                    ) as JSXElement,
+                    __emptyTraceMap__,
+
+                    [],
+                ).templateString,
+            ).toMatchInlineSnapshot(`
+              "
+                <!---->
+                <!---->
+                <span> <!----> </span>
+                <p> <!----> </p>
+              "
+            `);
+        });
     });
 });
