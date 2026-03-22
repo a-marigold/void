8b307fe36cd7a0a1529f24efaa858334cfe534b3
NextKolya <phelixgluy@gmail.com>
2026-03-22 16:35:39 +0600

    add tests for markParentsDynamic

--- packages/void/src/compiler/__tests__/transformer/jsx/utils.test.ts
+++ packages/void/src/compiler/__tests__/transformer/jsx/utils.test.ts
@@ -1,12 +1,18 @@
 import { describe, it, expect } from 'bun:test';
 
+import { parseExpression } from '@babel/parser';
+
 import { generate } from '@babel/generator';
+
+import type { JSXElement } from '@babel/types';
 
 import {
     generateChildPath,
     generateSiblingPath,
+    markParentsDynamic,
 } from '../../../transformer/jsx';
 
+import type { AnalyzeJSXResult } from '../../../transformer/types';
 describe('generateChildPath', () => {
     it('should return `parentName.firstChild` if `childIndex` is `0`', () => {
         expect(
@@ -39,5 +45,36 @@ describe('generateSiblingPath', () => {
         ).toMatchInlineSnapshot(
             `"anchor.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling"`,
         );
+    });
+});
+
+describe('markParentsDynamic', () => {
+    it('should add all the parents of `node` to `dynamicNodes`', () => {
+        const div = parseExpression(
+            `<div> 
+  <header> <span>{'dynamic'}</span> </header>
+</div>`,
+            { plugins: ['jsx'] },
+        ) as JSXElement;
+
+        const header = div.children[0] as JSXElement;
+        const span = header.children[0] as JSXElement;
+        const dynamicText = span.children[0];
+
+        const parents = new WeakMap([
+            [header, div],
+            [span, header],
+            [dynamicText, span],
+        ]);
+
+        const dynamicNodes: AnalyzeJSXResult['dynamicNodes'] = new Set();
+
+        markParentsDynamic(dynamicText, parents, dynamicNodes);
+
+        expect(
+            [div, header, span].every((parent) => {
+                return dynamicNodes.has(parent);
+            }),
+        ).toBe(true);
     });
 });
