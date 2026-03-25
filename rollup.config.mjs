e7b3b7ed6f301ceb30eea61633e00cdd96530213
NextKolya <phelixgluy@gmail.com>
2026-03-25 16:10:27 +0600

    remove compiler and reactivity to separated packages

---
+++ rollup.config.mjs
@@ -0,0 +1,34 @@
+import { readdirSync } from 'fs';
+
+import { defineConfig } from 'rollup';
+
+import typescript from '@rollup/plugin-typescript';
+
+import dts from 'rollup-plugin-dts';
+
+const PACKAGES_DIR_NAME = 'packages';
+
+const packageDirNames = readdirSync(PACKAGES_DIR_NAME);
+
+export default packageDirNames.flatMap((name) => {
+    const packagePath = PACKAGES_DIR_NAME + '/' + name;
+
+    return defineConfig([
+        {
+            input: packagePath + '/src/index.ts',
+            external: ['__tests__'],
+
+            treeshake: 'recommended',
+
+            plugins: [typescript({ exclude: ['**/__tests__/**'] })],
+
+            output: { file: packagePath + '/dist/index.js', format: 'esm' },
+        },
+        {
+            input: packagePath + '/src/index.ts',
+
+            plugins: [dts()],
+            output: { file: packagePath + '/dist/index.d.ts', format: 'esm' },
+        },
+    ]);
+});
