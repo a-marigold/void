560be06cc05e04beffd25983e1b4a40e16f4b18e
NextKolya <phelixgluy@gmail.com>
2026-04-06 19:22:31 +0600

    add new __testing utils__, rename some __testing utils__ with 'mock' prefix in transformer tets

--- packages/compiler/src/__tests__/phases/transformer/__testingUtils__.ts
+++ packages/compiler/src/__tests__/phases/transformer/__testingUtils__.ts
@@ -1,89 +1,98 @@
-import type * as types from '@babel/types';
+import type { Node } from 'oxc-parser';
+
+import { print } from 'esrap';
+import ts from 'esrap/languages/ts';
+import tsx from 'esrap/languages/tsx';
+import type { Visitors } from 'esrap';
 
 import MagicString from 'magic-string';
 import { TraceMap } from '@jridgewell/trace-mapping';
 import type { EncodedSourceMap } from '@jridgewell/trace-mapping';
 
 import type { PreprocessResult } from '../../../phases/preprocessor';
+
 import type { RuntimeApiName } from '../../../types';
 
+import type { ErrorContext } from '../../../phases/transformer/types';
+
 /**
- *
- * Returns `Map` with unique runtime API names ({@link PreprocessResult.runtimeApiNames}) as if it was created by preprocessor.
+ * Returns {@link PreprocessResult.runtimeApiNames} with unique runtime API names as if it was created by preprocessor.
  *
  *
  *
  * Used to imitate results from preprocessor in transformer tests.
  *
- *
  * @returns {Map} {@link PreprocessResult.runtimeApiNames}.
  */
 
-export const generateRuntimeApiNames =
-    (): PreprocessResult['runtimeApiNames'] => {
-        const runtimeApiNames: PreprocessResult['runtimeApiNames'] = new Map();
+export const mockRuntimeApiNames = (): PreprocessResult['runtimeApiNames'] => {
+    const runtimeApiNames: Record<string, string> = {};
 
-        for (const name of [
-            'Signal',
+    for (const name of [
+        'Signal',
+        'getValue',
+        'setValue',
+        'postSetValue',
+        'createEffect',
+        'compute',
+        'createComputation',
+    ] satisfies RuntimeApiName[]) {
+        runtimeApiNames[name] = '$_$' + name;
+    }
 
-            'getValue',
-
-            'setValue',
-
-            'postSetValue',
-
-            'createEffect',
-
-            'compute',
-
-            'createComputation',
-        ] satisfies RuntimeApiName[]) {
-            runtimeApiNames.set(name, '_$1610$_' + name);
-        }
-
-        return runtimeApiNames;
-    };
+    return runtimeApiNames as PreprocessResult['runtimeApiNames'];
+};
 export const __emptySourceMap__ = new MagicString('').generateMap();
 
 export const __emptyTraceMap__ = new TraceMap(
     __emptySourceMap__ as EncodedSourceMap,
 );
 
-export const createEmptyNodeLocation = (): types.SourceLocation => {
-    return {
-        start: { line: 1, column: 1, index: 1 },
-
-        end: { line: 1, column: 1, index: 1 },
-
-        filename: '',
-
-        identifierName: '',
-    };
-};
-
 /**
  *
  * Creates `preprocess` function result with empty filled properties (like `errors` are just an empty array and `sourceMap` is an empty source map).
  *
- *
  * @param overrides Properties of {@link PreprocessResult} that override empty filled properties.
- *
  *
  * @returns An imitation of `preprocess` function call.
  *
+ *
  */
-export const createPreprocessResult = (
+export const mockPreprocessResult = (
     overrides: Partial<PreprocessResult>,
 ): PreprocessResult => ({
     code: '',
     sourceMap: __emptySourceMap__,
     errors: [],
 
-    assignableLabels: new Map(),
-    unassignableLabels: new Map(),
+    assignableLabels: {},
+    unassignableLabels: {},
     identifiers: new Set(),
 
-    runtimeApiNames: overrides.runtimeApiNames ?? generateRuntimeApiNames(),
+    runtimeApiNames: overrides.runtimeApiNames ?? mockRuntimeApiNames(),
 
+    ...overrides,
+});
+
+/**
+ *
+ * Generates `node` from AST to TSX.
+ *
+ *
+ * @param node node to be generated.
+ *
+ */
+
+export const generate = (node: Node): string =>
+    print<Node>(node, Object.assign({}, ts(), tsx()) as Visitors<Node>, {
+        indent: '',
+    }).code;
+
+export const mockErrorContext = (
+    overrides: Partial<ErrorContext>,
+): ErrorContext => ({
+    errors: [],
+    traceMap: __emptyTraceMap__,
+    lineIndexes: [],
     ...overrides,
 });
