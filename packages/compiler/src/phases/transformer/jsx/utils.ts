c9c0d1dc21ec53f8e67930e3bf68f52e3b909f15
marigold <phelixgluy@gmail.com>
2026-06-23 15:20:56 +0600

    add: createReactiveExprInit jsx util. refactor: createReactiveInsertCall jsx util

--- packages/compiler/src/phases/transformer/jsx/utils.ts
+++ packages/compiler/src/phases/transformer/jsx/utils.ts
@@ -1,80 +1,101 @@
 import type {
 	ArrowFunctionExpression,
-	IdentifierName as Identifier,
 	Expression,
 	CallExpression,
-	NullLiteral,
+	VariableDeclaration,
+	AssignmentExpression,
 } from 'oxc-parser';
 
-import type { UniqueId } from '../../preprocessor';
+import { type PreprocessResult, type UniqueId } from '../../preprocessor';
 import * as nodes from '../nodes';
 import { createEffectInit } from '../utils';
 
+import { TEXT_NODE_DATA_KEY_NAME } from './constants';
 import type { ComponentChildren, ComponentProps } from './types';
 
 /**
  * @param expr Expression for first argument of `insert`.
  * @param anchorIdName Name of identifier `anchor` argument of `insert`.
- * @param prevExpr `prevExpr` argument of `insert` runtime function.
- * @param insertName `insert` of {@link PreprocessResult.runtimeApiNames}
+ * @param insertName `insert` of {@link PreprocessResult.runtimeApiNames}.
  *
  * @returns Call of `insert` - `insert(expr, anchorIdName, prevExprNode)`.
  */
 export const createInsertCall = (
 	expr: Expression,
 	anchorIdName: UniqueId,
-	prevExpr: Identifier | NullLiteral,
 	insertName: UniqueId,
 ): CallExpression =>
 	nodes.callExpression(
 		nodes.identifier(insertName),
-
-		[expr, nodes.identifier(anchorIdName), prevExpr],
+		[expr, nodes.identifier(anchorIdName)],
 		null,
 	);
-/**
- *
- * @param expr Expression for first argument of `insert`.
- * @param anchorIdName Name of identifier of `anchor` argument of `insert`.
- * @param prevExprIdName Name of identifier of `prevExprNode` for `insert`.
- * @param insertName `insert` of {@link PreprocessResult.runtimeApiNames}.
- * @param createEffectName `createEffect` of {@link PreprocessResult.runtimeApiNames}.
- *
- *
- *
- *
- * @returns Call of `createEffect` with insertion - `createEffect(() => prevExprIdName = insert(expr,anchorIdName,prevExprIdName)`
- */
 
-export const createReactiveInsertCall = (
+/**
+ * @param initNodeIdName Name of identifier to be used in initialization.
+ * @param expr Init expression to be insterted.
+ * @param anchorIdName For {@link createInsertCall}.
+ * @param insertName For {@link createInsertCall}.
+ *
+ * @returns `const (initNodeIdName) = insert(expr, (anchorIdName));`.
+ */
+export const createReactiveExprInit = (
+	initNodeIdName: UniqueId,
 	expr: Expression,
 	anchorIdName: UniqueId,
-	prevExprIdName: UniqueId,
 	insertName: UniqueId,
+): VariableDeclaration =>
+	nodes.variableDeclaration('const', [
+		nodes.variableDeclarator(
+			nodes.identifier(initNodeIdName),
+
+			createInsertCall(expr, anchorIdName, insertName),
+		),
+	]);
+
+/**
+ *
+ * @param expr Expression to be inserted.
+ * @param initNodeIdName Name of identifier used in {@link createReactiveExprInit} for this expression.
+ * @param createEffectName Name of `createEffect` runtime function.
+ *
+ * @returns Effect init with reusing prev node via updating its data ({@link createTextNodeReuse}).
+ */
+export const createReactiveInsertCall = (
+	expr: Expression,
+	initNodeIdName: UniqueId,
 	createEffectName: UniqueId,
 ): CallExpression =>
 	createEffectInit(
-		nodes.arrowFunction(
-			nodes.assignmentExpression(
-				'=',
-				nodes.identifier(prevExprIdName),
-				createInsertCall(
-					expr,
-					anchorIdName,
-					nodes.identifier(prevExprIdName),
-					insertName,
-				),
-			),
-			[],
-		),
-
+		nodes.arrowFunction(createTextNodeReuse(expr, initNodeIdName), []),
 		createEffectName,
+	);
+
+/**
+ * @param expr Expression with string or number.
+ * @param initNodeIdName Name of identifier used in {@link createReactiveExprInit} for this expression.
+ *
+ * @returns `(prevNodeIdName).data = (expr);`.
+ */
+export const createTextNodeReuse = (
+	expr: Expression,
+	initNodeIdName: UniqueId,
+): AssignmentExpression =>
+	nodes.assignmentExpression(
+		'=',
+
+		nodes.memberExpression(
+			nodes.identifier(initNodeIdName),
+			nodes.identifier(TEXT_NODE_DATA_KEY_NAME),
+		),
+		expr,
 	);
 
 /**
  * @param componentFnIdName Name of identifier of `fn` argument of `createComponent`.
  * @param props {@link ComponentProps}.
  * @param createComponentName Name of `createComponent` runtime function.
+ *
  *
  * @returns Call of `createComponent` runtime function.
  */
@@ -111,8 +132,6 @@ export const createComponentInsertCall = (
 	createInsertCall(
 		createComponentInit(componentFnIdName, props, createComponentName),
 		anchorIdName,
-		nodes.literal<NullLiteral>(null),
-
 		insertName,
 	);
 
